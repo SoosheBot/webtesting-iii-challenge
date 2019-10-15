bbae857ecd931fb227e20cc459dfd7bb9c62f2be
sooshe <sooshe@sooshebot.com>
2019-10-15 15:14:32 -0500

    did not complete mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -9,8 +9,8 @@ test('should match snapshot', () => {
     expect(render(<Controls />)).toMatchSnapshot();
 });
 
-test('controls displays in dashboard', () => {
+test('controls displays', () => {
     const { getByTestId } = render(<Controls />);
     expect(getByTestId('controls'));
-})
+});
 
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+
+test('should match snapshot', () => {
+    expect(render(<Controls />)).toMatchSnapshot();
+});
+
+test('controls displays', () => {
+    const { getByTestId } = render(<Controls />);
+    expect(getByTestId('controls'));
+});
+
