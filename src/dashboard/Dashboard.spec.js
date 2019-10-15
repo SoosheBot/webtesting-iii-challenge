52444bf8ef6183169a3a596352fd72fbe3593db2
sooshe <sooshe@sooshebot.com>
2019-10-15 09:05:04 -0500

    write first snapshot and test that it works by changing code

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,3 +1,9 @@
-// Test away
 import React from 'react';
+import { render } from '@testing-library/react';
+
 import Dashboard from './Dashboard';
+import { exportAllDeclaration } from '@babel/types';
+
+test('should match snapshot', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+});
