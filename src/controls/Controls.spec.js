9ba5837d671a293dc2feb6b27136d6f3a32211c9
sooshe <sooshe@sooshebot.com>
2019-10-15 09:26:09 -0500

    add test to controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Controls from './Controls';
+
+
+test('should match snapshot', () => {
+    expect(render(<Controls />)).toMatchSnapshot();
+});
+
+test('controls displays in dashboard', () => {
+    const { getByTestId } = render(<Controls />);
+    expect(getByTestId('controls'));
+})
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Dashboard from './Dashboard';
-import { exportAllDeclaration } from '@babel/types';
 
 test('should match snapshot', () => {
     expect(render(<Dashboard />)).toMatchSnapshot();
 });
+
